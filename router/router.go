997064b3aa72e37489dc96a67b6f40a2b04f86f3
bleedcloudVIII <yurikaras8@gmail.com>
2023-02-18 15:05:39 +1000

    Change url api

--- router/router.go
+++ router/router.go
@@ -11,11 +11,11 @@ import (
 func StartRouter() {
 	log.Println("Starting the HTTP server on port 8090")
 	router := mux.NewRouter()
-	router.HandleFunc("/create", service.CreatePerson).Methods("POST")
-	router.HandleFunc("/get/{id}", service.GetPersonByID).Methods("GET")
-	router.HandleFunc("/get", service.GetAllPerson).Methods("GET")
-	router.HandleFunc("/update/{id}", service.UpdatePersonByID).Methods("PUT")
-	router.HandleFunc("/delete/{id}", service.DeletPersonByID).Methods("DELETE")
+	router.HandleFunc("/users", service.CreatePerson).Methods("POST")
+	router.HandleFunc("/users/{id}", service.GetPersonByID).Methods("GET")
+	router.HandleFunc("/users", service.GetAllPerson).Methods("GET")
+	router.HandleFunc("/users/{id}", service.UpdatePersonByID).Methods("PUT")
+	router.HandleFunc("/users/{id}", service.DeletPersonByID).Methods("DELETE")
 	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(":8090", router))
 
